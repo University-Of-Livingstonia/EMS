7f7f402d8aa1e81ad93cab4bb920f81b49eb7d6f
isaac precious chipeta <chipetaisaac@gmail.com>
2025-07-01 16:23:23 +0200

    refactor : moved secrets to .env

--- config/email.php
+++ config/email.php
@@ -1,11 +1,12 @@
 <?php
+$dotenv = parse_ini_file(__DIR__ . '/../.env');
 return [
-    'host' => 'smtp.gmail.com',
-    'username' => 'events@unilia.ac.mw', 
-    'password' => 'kchu anzj nack drgn', 
-    'port' => 587,
-    'encryption' => 'tls',
-    'from_email' => 'events@unilia.ac.mw',
+    'host' => $dotenv['SMTP_HOST'],
+    'username' => $dotenv['SMTP_USERNAME'],
+    'password' => $dotenv['SMTP_PASSWORD'],
+    'port' => $dotenv['SMTP_PORT'],
+    'encryption' => $dotenv['SMTP_ENCRYPTION'],
+    'from_email' => $dotenv['SMTP_USERNAME'],
     'from_name' => 'UNILIA Events System'
 ];
 ?>
